c546dd72c0199d2c6a6fc96c89f4e7e4a892977d
swiftyy188 <jeffho.ml@gmail.com>
2018-04-04 15:55:23 -0400

    Mongoose added

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended': 'false'}));
 
+//Mongoose
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+mongoose.connect('mongodb://localhost/astarter', {promiseLibrary:
+require('bluebird') })
+	.then(() => console.log('connection successful'))
+	.catch((err) => console.error(err))
+
 //catch 404 and forward to error handler
 app.use(function(req, res, next){
 	var err = new Error('Not Found');
